d9f1d68e43f4abaa53631be50a5e8a5e1a60825d
paolanocom <paolanocom@gmail.com>
2023-10-17 10:40:22 +0800

    Fix: Filename error

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavMenu from './NavMenu';
 import Link from 'next/link';
 
 const NavBar = () => {
+  const [signedIn, setSignedIn] = useState(false)
   return (
     <nav className='flex flex-row px-6 py-3 justify-between items-center w-full'>
       <div className='flex gap-3'>
@@ -11,11 +12,13 @@ const NavBar = () => {
       </div>
       {/* <div>NavCenter</div> */}
       <div className='flex gap-4'>
-        <button className='rounded px-3 py-1 border border-pandesal-orange text-pandesal-orange'>
+        {!signedIn && 
+          <button className='rounded px-3 py-1 border border-pandesal-orange text-pandesal-orange hover:text-white hover:bg-pandesal-orange/80 duration-100'>
           <Link href='/#signup-form'>
             Log In
           </Link>
         </button>
+        }
       </div>
     </nav>
   )
